09cd9ca95c5d1cbb28250cdf36d5d0d3f26720f0
Tima <timathevedmed@gmail.com>
2022-06-08 13:07:46 +0200

    Use new trip images

--- index.mjs
+++ index.mjs
@@ -5,8 +5,8 @@ const port = 8080;
 const app = express();
 
 const trips = [
-    {'id': 0, 'name': 'Szczyt wszystkiego', 'description': 'Krótka wycieczka z wejściem na ten właśnie szczyt.', 'price': 15002900, 'src': '/mountain', 'alt': 'mountain'},
-    {'id': 1, 'name': 'Dalekie morza', 'description': 'Mórz jest wiele, więc i opis może być nieco dłuższy niż poprzednio. Atrakcji też może być więcej.', 'price': 17, 'src': '/sea.webp', 'alt': 'sea'},
+    {'id': 0, 'name': 'Szczyt wszystkiego', 'description': 'Krótka wycieczka z wejściem na ten właśnie szczyt.', 'price': 15002900, 'src': '/mountain.jpg', 'alt': 'mountain'},
+    {'id': 1, 'name': 'Dalekie morza', 'description': 'Mórz jest wiele, więc i opis może być nieco dłuższy niż poprzednio. Atrakcji też może być więcej.', 'price': 17, 'src': '/sea.jpeg', 'alt': 'sea'},
     {'id': 2, 'name': 'Miasto', 'description': 'Na świecie mamy jeszcze miasta, można je zwiedzać.', 'price': 3405691582, 'src': '/city.jpg', 'alt': 'city'}
 ];
 
