3d654384d29c840ff6c7cf146bbc1ffd985fd810
Tima <timathevedmed@gmail.com>
2022-06-09 11:00:12 +0200

    Implement script for db initialization

--- database/connection.mjs
+++ database/connection.mjs
@@ -1,7 +1,26 @@
 import { Sequelize } from 'sequelize';
+import { createTripTable, createReservationTable } from './models.mjs'
 
 const sequelize = new Sequelize('db', 'user', 'pass', {
     host: 'localhost',
     dialect: 'sqlite',
     storage: 'db.sqlite'
 });
+
+export default async function connect() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connected to database successfully.');
+
+        const Trip = await createTripTable(sequelize);
+        const Reservation = await createReservationTable(sequelize);
+
+        Trip.hasMany(Reservation);
+        Reservation.belongsTo(Trip);
+
+        return {Trip, Reservation, sequelize};
+    } catch (error) {
+        console.log(`Failed to connect to database. ${error.message}`);
+        throw error;
+    }
+};
--- database/models.mjs
+++ database/models.mjs
@@ -1,7 +1,6 @@
 import { DataTypes} from 'sequelize';
-import { sequelize } from './db.mjs'
 
-const Trip = sequelize.define('Trip', {
+export const createTripTable = (sequelize) => sequelize.define('Trip', {
     id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
@@ -15,7 +14,7 @@ const Trip = sequelize.define('Trip', {
         type: DataTypes.TEXT,
         defaultValue: ""
     },
-    img: {
+    image: {
         type: DataTypes.STRING,
         allowNull: false
     },
@@ -54,7 +53,7 @@ const Trip = sequelize.define('Trip', {
     }
 });
 
-const Reservation = sequelize.define('Reservation', {
+export const createReservationTable = (sequelize) => sequelize.define('Reservation', {
     id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
@@ -85,47 +84,11 @@ const Reservation = sequelize.define('Reservation', {
         type: DataTypes.STRING,
         allowNull: false
     },
-    bought: {
+    tickets: {
         type: DataTypes.INTEGER,
         allowNull: false,
         validate: {
             min: 1
         }
     }
-})
-
-await Wycieczki.sync({ force: true });
-await Zgloszenia.sync({ force: true });
-
-await Wycicezki.create({
-    'nazwa': 'Szczyt wszystkiego',
-    'opis': 'Krótka wycieczka z wejściem na ten właśnie szczyt.',
-    'cena': 15002900,
-    'obrazek': '/mountain',
-    'alt': 'mountain',
-    'data_poczatku': '17-01-2022',
-    'data_konca': '20-01-2022',
-    'liczba_dostepnych_miejsc': 5
-}); 
-
-await Wycicezki.create({
-    'nazwa': 'Dalekie morza',
-    'opis': 'Mórz jest wiele, więc i opis może być nieco dłuższy niż poprzednio. Atrakcji też może być więcej.',
-    'cena': 17,
-    'obrazek': '/sea.webp',
-    'alt': 'sea',
-    'data_poczatku': '02-03-2022',
-    'data_konca': '05-03-2022',
-    'liczba_dostepnych_miejsc': 1
-}); 
-
-await Wycicezki.create({
-    'nazwa': 'Miasto',
-    'opis': 'Na świecie mamy jeszcze miasta, można je zwiedzać.',
-    'cena': 3405691582,
-    'obrazek': '/city.jpg',
-    'alt': 'city',
-    'data_poczatku': '23-05-2022',
-    'data_konca': '24-01-2022',
-    'liczba_dostepnych_miejsc': 23
-}); 
+});
